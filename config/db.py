5dcd4086d4a6d01677911acc07e13464f817dcf9
jdpalmad <juandpalmad@gmail.com>
2023-02-12 22:56:27 -0500

    env variables added

--- config/db.py
+++ config/db.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, MetaData
 
 # Configuracion de la conexion a la base de datos
+# Valores de ejemplo, cambiar por los valores de su base de datos
 user = "root"
 sql_password = "sqlpw123"
 database = "eventsdb"
