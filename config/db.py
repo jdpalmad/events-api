7a49d4ffe5cc3e061d661776aa6b0f54618a3d9b
jdpalmad <juandpalmad@gmail.com>
2023-02-12 21:07:24 -0500

    DB Connection and settings

---
+++ config/db.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine, MetaData
+
+# Configuracion de la conexion a la base de datos
+user = "root"
+sql_password = "sqlpw123"
+database = "eventsdb"
+host = "localhost"
+port = "3306"
+
+engine = create_engine(f"mysql+pymysql://"+user+":"+sql_password+"@"+host+":"+port+"/"+database)
+
+meta = MetaData()
+conn = engine.connect()
+
+
