49a7191ac65c0a85c29d4412e7a924b4ae95ee25
jdpalmad <juandpalmad@gmail.com>
2023-03-06 16:38:43 -0500

    Gestion microservise fixed 0.0.1b

--- routes/gestion.py
+++ routes/gestion.py
@@ -2,23 +2,16 @@ from fastapi import APIRouter, HTTPException
 from config.db import conn
 from models.event import events
 from models.gestion import gestion
+from models.cfg import config
 from schemas.gestion import gestionEvent
 from schemas.event import Event
 from utilities.gestion import *
+from sqlalchemy import select, join
 
 # Creamos el router para gestion
-gestion = APIRouter()
+gestionr = APIRouter()
 
-@gestion.get("/events/gestion", response_model=list[gestionEvent], description="""Obtenga el estado de gestion
-    de todos los eventos""", tags=["gestion"])
-def get_gestion_events():
-    # Obtenga una lista de todos los 
-    # eventos activos y su estado de gestion
-    # vea mas en utilities/gestion.py
-    return conn.execute(MostrarGestionActivos).mappings().all()
-
-
-@gestion.get("/events/gestion/{id}", response_model=gestionEvent, description="""Obtenga el estado de 
+@gestionr.get("/events/gestion/{id}", response_model = gestionEvent ,description="""Obtenga el estado de 
     gestion de un evento""", tags=["gestion"])   
 def get_gestion_status_by_id(id:int):
     # Vea si el evento existe, si no, lance una excepción, si existe, evalue la funcion gestion_status
@@ -32,7 +25,22 @@ def get_gestion_status_by_id(id:int):
         # Si el evento no existe, lance una excepción
         raise HTTPException(status_code=404, detail="El evento no existe")
 
-@gestion.get("/gestion/{clause}", response_model=list[Event], description="""Obtenga el estado de
+
+@gestionr.get("/gestion", response_model = list[gestionEvent] ,description="""Obtenga el estado de gestion
+    de todos los eventos""", tags=["gestion"])
+def get_gestion_events():
+    # Obtenga una lista de todos los 
+    # eventos activos y su estado de gestion
+    # vea mas en utilities/gestion.py
+    return conn.execute(select(
+    gestion.c.id, gestion.c.event_id, gestion.c.nombre, gestion.c.tipo, gestion.c.requiere_gestion
+).select_from(
+    join(gestion, config, gestion.c.event_id == config.c.event_id)
+).where(
+    config.c.active == 1
+)).mappings().all()
+
+@gestionr.get("/gestion/{clause}", response_model=list[Event], description="""Obtenga el estado de
     gestion de todos los eventos""", tags=["gestion"])
 def check_gestion(clause:bool):
     # Hace las correspondientes verificaciones para recuperar los eventos
@@ -42,3 +50,5 @@ def check_gestion(clause:bool):
     elif clause == False:
         return conn.execute(MostrarEventosNoRequiereGestion).mappings().all()
     # Vea mas en utilities/gestion.py
+
+
